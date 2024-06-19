4652a08784fd7777f4838d718d7dd96d8020fcd5
kingola1 <segun8427@gmail.com>
2024-06-19 10:04:43 +0100

    started who we are page

--- resources/views/who-we-are.blade.php
+++ resources/views/who-we-are.blade.php
@@ -8,39 +8,44 @@
     </div>
     <div class="bg-white">
         <div class="mx-20 py-20">
-            <div class="flex flex-row gap-28">
-                <div class="basis-3/7 pt-6 text-xs poppins-regular">
+            <div class="flex flex-col lg:flex-row items-center gap-12 xl:gap-28">
+                <div class="basis-1/2 pt-6 md:text-sm text-xs xl:text-lg xl:leading-[24px] poppins-thin">
                     <p class="pb-4 border-[#25A8D6] pl-5 border-l-[14px]">
-                        The Special Foundation is a social impact
-                        organisation that focuses on raising young African
-                        leaders through education by providing platforms and
-                        opportunities to fulfil their dreams. We are a
-                        community of young professionals with a vision to
-                        improve the quality of leadership in all spheres of
-                        governance in Africa by creating access for more
-                        children to get an education.
+                        The Special Foundation is a social impact organisation that focuses on raising young African
+                        leaders through education by providing platforms and opportunities to fulfil their dreams. We
+                        are a community of young professionals with a vision to improve the quality of leadership in all
+                        spheres of governance in Africa by creating access for more children to get an education.
                     </p>
                     <p class="border-[#25A8D6] pl-5 border-l-[14px]">
-                        Founded by Seyi Akinwale in 2018 through his vision
-                        to improve the quality of leadership in all spheres
-                        of governance in Africa and ensure the creation of a
-                        platform where more young people can maximize their
-                        potential by gaining access to education. The
-                        foundation has empowered over 21,000 less
-                        privileged, gifted African Children through
-                        education, mentorship, and leadership opportunities.
+                        Founded by Seyi Akinwale in 2018 through his vision to improve the quality of leadership in all
+                        spheres of governance in Africa and ensure the creation of a platform where more young people
+                        can maximize their potential by gaining access to education. The foundation has empowered over
+                        21,000 less privileged, gifted African Children through education, mentorship, and leadership
+                        opportunities.
                     </p>
                 </div>
-                <div class="basis-4/7 pl-6">
-                    <img src="/images/obasanjo.png" alt="" />
+                <div class="basis-1/2 pl-6">
+                    <img src="/images/who_we_are_1.png" alt="" />
                 </div>
             </div>
-            <div class="flex flex-row pt-20">
-                <div class="basis-3/7 gap-12 flex flex-col">
-                    <div class="flex flex-col bg-[#25A8D6] text-white mx-10 py-6 w-96">
+            <div class="flex flex-row pt-20 gap-12 xl:gap-28">
+                <div class="basis-1/2 gap-12 flex flex-col">
+                    <div class="flex flex-col bg-[#25A8D6] text-white p-16 rounded-2xl shadow-sm gap-6">
                         <img class="w-16" src="/images/goal-icon.png" alt="" />
-                        <h1 class="text-2xl">Our Mission</h1>
-                        <p class="text-[8px] poppins-light">
+                        <h1 class="text-4xl poppins-bold">Our Mission</h1>
+                        <p class="text-[8px] poppins-light lg:leading-6 xl:text-lg">
+                            To improve the quality of leadership in Africa
+                            by providing a platform where less privileged
+                            and highly talented children can be equipped
+                            with an education, mentoring and leadership
+                            training to bring about a positive influence to
+                            their world.
+                        </p>
+                    </div>
+                    <div class="flex flex-col bg-[#25A8D6] text-white p-16 rounded-2xl shadow-sm gap-6">
+                        <img class="w-16" src="/images/goal-icon.png" alt="" />
+                        <h1 class="text-4xl poppins-bold">Our Vision</h1>
+                        <p class="text-[8px] poppins-light lg:leading-6 xl:text-lg">
                             To improve the quality of leadership in Africa
                             by providing a platform where less privileged
                             and highly talented children can be equipped
@@ -50,7 +55,42 @@
                         </p>
                     </div>
                 </div>
-                <div class="basis-4/7 flex"></div>
+                <div class="basis-1/2 flex gap-12">
+                    <div class="flex flex-col">
+                        <h2 class="text-[#25A8D6] poppins-bold xl:text-5xl">Our Numbers</h2>
+                        <div class="flex flex-col">
+                            <h3 class="poppins-bold text-3xl">21,000+</h3>
+                            <p></p>
+                        </div>
+                        <div class="flex flex-col">
+                            <h3 class="poppins-bold text-3xl">21,000+</h3>
+                            <p></p>
+                        </div>
+                        <div class="flex flex-col">
+                            <h3 class="poppins-bold text-3xl">21,000+</h3>
+                            <p></p>
+                        </div>
+                        <div class="flex flex-col">
+                            <h3 class="poppins-bold text-3xl">21,000+</h3>
+                            <p></p>
+                        </div>
+                        <div class="flex flex-col">
+                            <h3 class="poppins-bold text-3xl">21,000+</h3>
+                            <p></p>
+                        </div>
+                    </div>
+                    <div class="flex flex-col">
+                        <br>
+                        <img src="/images/arrow.svg" alt="">
+                        <img src="/images/arrow.svg" alt="">
+                        <img src="/images/arrow.svg" alt="">
+                        <img src="/images/arrow.svg" alt="">
+                        <img src="/images/arrow.svg" alt="">
+                    </div>
+                    <div class="flex flex-col">
+                        <h2 class="text-[#25A8D6] poppins-bold xl:text-5xl">Our 2030 Target</h2>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
