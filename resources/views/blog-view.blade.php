b5ca0a2e5e8245e49007709443016b2e6f8a5146
bloom-ng <it@bloomdigitmedia.com>
2024-06-25 20:33:34 +0000

    completed single blog page view

---
+++ resources/views/blog-view.blade.php
@@ -0,0 +1,204 @@
+<x-guest-layout title="Special Foundation - Blogs" page="blog">
+    <div class="relative bg-cover bg-center h-40"
+        style="background-image: url('{{ asset('/images/rectangle-background.png') }}');">
+        <div class="h-full bg-[#26225F]/90">
+
+        </div>
+    </div>
+
+    {{-- BLOG TITLE START --}}
+    <div class="w-full flex items-center justify-center p-8 lg:p-10">
+        <h1
+            class="font-montserrat text-4xl font-extrabold leading-none text-center md:text-5xl md:leading-tight lg:text-6xl lg:leading-none xl:text-7xl xl:leading-none text-[#25A8D6] my-12 xl:w-[60%]">
+            Children have a right to an education, a quality education.
+        </h1>
+    </div>
+    {{-- BLOG TITLE END --}}
+
+    <div class="w-full flex flex-col p-8 lg:p-20 gap-14">
+        {{-- BLOG AUTHOR & SHARE START --}}
+        <div class="w-full flex flex-col lg:flex-row items-center justify-between bg-[#26225F] px-6 lg:px-16 py-4">
+            <div>
+                <h3
+                    class="montserrat-thin text-base font-normal leading-6 text-left md:text-lg md:leading-7 lg:text-xl lg:leading-8 xl:text-2xl xl:leading-9 text-white">
+                    Written by <span
+                        class="poppins-bold text-base font-black leading-6 text-left md:text-lg md:leading-7 lg:text-xl lg:leading-8 xl:text-2xl xl:leading-9 text-white">
+                        David Aremu
+                    </span>
+
+                </h3>
+
+            </div>
+
+            <div class="flex items-center justify-center gap-2">
+                <p
+                    class="font-montserrat text-base font-normal leading-6 text-left md:text-lg md:leading-7 lg:text-xl lg:leading-8 xl:text-2xl xl:leading-9 text-white">
+                    Share |</p>
+                <div class="flex items-center justify-center gap-4">
+                    <img src="/images/blog_facebook.svg" alt="FaceBook Logo">
+                    <img src="/images/blog_twitter.svg" alt="Twitter Logo">
+                    <img src="/images/blog_instagram.svg" alt="Instagram Logo">
+                    <img src="/images/blog_linkedin.svg" alt="Linkedin Logo">
+                </div>
+            </div>
+        </div>
+        {{-- BLOG AUTHOR & SHARE START --}}
+
+        {{-- BLOG CONTENT START --}}
+        <p
+            class="montserrat-light text-base font-normal leading-6 text-left md:text-lg md:leading-7 lg:text-xl lg:leading-8 xl:text-2xl xl:leading-9">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius ex augue, non hendrerit nisi fermentum
+            id. Aliquam facilisis vel ante id porttitor. Mauris rhoncus maximus purus, vel cursus dolor pellentesque at.
+            Phasellus egestas efficitur felis nec ultricies. Donec purus quam, porttitor in rhoncus eget, mattis a
+            dolor. Vivamus auctor maximus arcu vel accumsan. Vivamus pellentesque gravida arcu, et tristique purus
+            vestibulum vitae. Nam eget sapien ut mi euismod mollis vel eget tortor.
+            <br>
+            <br>
+
+            Curabitur lectus neque, hendrerit ut quam lacinia, laoreet ultricies ex. Cras sit amet urna et nulla feugiat
+            consectetur sit amet ac neque. Ut eget finibus purus. Cras vitae suscipit ex. Praesent blandit mi eu maximus
+            tincidunt. Pellentesque eleifend dapibus justo, vitae feugiat eros rhoncus quis. Phasellus eget nisi
+            pharetra, consequat urna non, aliquet orci. Nullam pretium, dolor eget maximus finibus, augue metus interdum
+            nulla, quis ornare est urna at nulla. Fusce et quam quis velit porttitor pharetra sit amet et magna. Nullam
+            placerat pretium nulla ac sodales. Vestibulum nisi risus, varius id mi sed, consequat facilisis odio.
+            <br>
+            <br>
+
+            Donec ut lectus vitae risus molestie sodales. Nullam viverra diam at sem maximus, ac ultricies sem accumsan.
+            Nullam fringilla iaculis sagittis. Aliquam ut mattis elit. Integer libero ante, eleifend vel porta quis,
+            cursus nec massa. Donec non pharetra risus, eu tristique tellus. Donec fringilla tincidunt arcu, id
+            consectetur eros bibendum auctor. Curabitur bibendum libero justo, id porttitor nibh faucibus vel. Cras
+            aliquam a massa vitae lobortis. Ut vitae odio mollis, posuere mauris sed, aliquet dui. Vestibulum nisi
+            ipsum, viverra sed eleifend pellentesque, porttitor quis purus. Phasellus euismod elementum volutpat. Nulla
+            porttitor lectus sem, quis sagittis massa vulputate non. Praesent imperdiet lectus vulputate, tincidunt ex
+            sit amet, consectetur erat.
+            <br>
+            <br>
+
+            Morbi libero lorem, egestas eget malesuada a, dictum a diam. Duis metus arcu, venenatis vitae nunc at,
+            laoreet accumsan libero. Nullam id erat vulputate urna accumsan pretium. Orci varius natoque penatibus et
+            magnis dis parturient montes, nascetur ridiculus mus. Praesent non diam condimentum, auctor magna sed,
+            volutpat risus. Suspendisse efficitur suscipit pulvinar. Integer quis lacus vel arcu dapibus bibendum congue
+            quis velit. Suspendisse potenti. Donec nec metus ut dui commodo rhoncus. Nunc a auctor lacus, non dignissim
+            quam. Morbi aliquet non nibh ac dignissim.
+            <br>
+            <br>
+
+            Proin volutpat purus vel euismod congue. Maecenas sed faucibus neque, sit amet consectetur lorem. Integer
+            dictum nibh varius ipsum gravida, non facilisis magna pellentesque. Mauris egestas condimentum interdum.
+            Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse sodales aliquam turpis, eget
+            semper metus. Morbi non tellus nisi. Vestibulum mollis mi eget odio hendrerit, eu hendrerit sem congue. Ut
+            imperdiet eros non auctor pretium. Vestibulum scelerisque nisl vitae lacus dictum eleifend.
+            <br>
+            <br>
+
+            Nunc vehicula id erat ut rutrum. Donec nec nibh quis lacus fermentum tempor. Vestibulum ante ipsum primis in
+            faucibus orci luctus et ultrices posuere cubilia curae; Proin quis cursus neque. Proin id aliquam neque.
+            Fusce at sapien nec leo maximus scelerisque vel feugiat diam. Cras fringilla posuere nulla, nec rutrum
+            risus. Nam non hendrerit lacus. Pellentesque sit amet elementum massa. Pellentesque laoreet tortor vitae
+            dolor iaculis maximus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus
+            mus. Nulla tempor dolor dui, vel lobortis arcu congue sit amet. Maecenas lobortis dapibus quam ac ultricies.
+            Nunc enim metus, fermentum non laoreet a, pellentesque at odio.
+            <br>
+            <br>
+
+            Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque sit amet ante
+            in sapien imperdiet congue. Sed at erat tempus, cursus nunc ut, aliquet ipsum. Donec a nulla nibh. Morbi
+            maximus porta hendrerit. Nam faucibus mollis dui posuere auctor. Donec eget nibh quis sem faucibus
+            elementum. Nunc commodo dolor quis finibus pharetra. Proin sagittis dictum magna non blandit. Vestibulum
+            quis libero massa. Donec convallis mollis neque vitae molestie.
+            <br>
+            <br>
+
+            Nulla malesuada ipsum nisi, quis luctus turpis porttitor vel. Nullam in pretium magna. Nullam at mollis
+            lorem, et tempor ligula. Donec interdum tincidunt lacus tincidunt malesuada. Suspendisse faucibus facilisis
+            tellus, quis porttitor ligula hendrerit id. Morbi ut elementum massa. In eu venenatis sapien, id lobortis
+            enim. Pellentesque dui dolor, aliquam at ante vitae, dignissim eleifend mi. Nullam pellentesque ante ut
+            metus feugiat imperdiet. Donec molestie rhoncus elit, eget vulputate libero consectetur sodales. Cras
+            rhoncus risus non gravida mattis.
+            <br>
+            <br>
+
+            Duis vel condimentum lorem. Morbi pretium sapien sit amet vehicula scelerisque. Proin feugiat nec magna nec
+            dictum. Quisque eget posuere neque. Donec ultrices ut libero ut interdum. Aenean sed dolor porttitor,
+            facilisis urna non, ultricies libero. Vivamus vitae metus eu arcu imperdiet condimentum in quis dolor.
+            Praesent vehicula, mi mollis dictum ornare, velit felis pharetra lorem, a cursus risus quam vitae nisi. Nam
+            massa sapien, eleifend quis purus sed, lobortis blandit felis. Sed at enim risus. Nullam vitae justo quis
+            magna molestie dictum a eget nisi. Sed tincidunt tortor ex, in varius tellus pellentesque at. Mauris laoreet
+            lacus nec eleifend ultrices. Sed at luctus dolor.
+            <br />
+            <br />
+
+            Suspendisse vel fringilla velit. Curabitur vitae enim ac nisi pulvinar consectetur non at lectus. Fusce
+            ultrices, purus et auctor dignissim, est diam rhoncus ipsum, eu commodo lectus dui sed felis. Proin quis
+            arcu ac quam suscipit interdum. Donec dignissim tristique dolor sed aliquam. Vestibulum scelerisque
+            malesuada dui sit amet commodo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames
+            ac turpis egestas. Morbi tortor nisl, suscipit sagittis mollis nec, imperdiet sed nibh. Nullam iaculis
+            eleifend dolor sed fermentum. Nullam sollicitudin sollicitudin mi. Nulla sed iaculis ipsum, eget egestas
+            est.
+        </p>
+        {{-- BLOG CONTENT END --}}
+    </div>
+
+    <div class="flex flex-col items-center justify-center p-8 lg:p-20 bg-[#25A8D6]">
+        <h2
+            class="font-montserrat text-4xl font-extrabold leading-none text-center md:text-5xl md:leading-tight lg:text-6xl lg:leading-none xl:text-7xl xl:leading-none mb-8 lg:mb-12 text-[#26225F]">
+            You may also like
+        </h2>
+
+        <div class="w-full flex flex-col lg:flex-row items-center justify-around gap-6 lg:gap-0">
+            <div class="flex flex-col bg-[#26225F] w-[380px]">
+                <div>
+                    <img class="w-[380px]" src="/images/teacher-giving-high-five.png" alt="" />
+                </div>
+                <div class="mx-12 pt-8 pb-5 text-white">
+                    <h1 class="pb-4 leading-[20px] poppins-semibold">
+                        Children have a right to an education, a quality
+                        <br />
+                        education.
+                    </h1>
+                    <p class="poppins-light text-xs pb-4">
+                        November 26, 2021
+                    </p>
+                    <a class="text-[#25A8D6] text-[8px]" href="/blog/1">READ MORE</a>
+                </div>
+            </div>
+
+            <div class="flex flex-col bg-[#26225F] w-[380px]">
+                <div>
+                    <img class="w-[380px]" src="/images/teacher-giving-high-five.png" alt="" />
+                </div>
+                <div class="mx-12 pt-8 pb-5 text-white">
+                    <h1 class="pb-4 leading-[20px] poppins-semibold">
+                        Children have a right to an education, a quality
+                        <br />
+                        education.
+                    </h1>
+                    <p class="poppins-light text-xs pb-4">
+                        November 26, 2021
+                    </p>
+                    <a class="text-[#25A8D6] text-[8px]" href="#">READ MORE</a>
+                </div>
+            </div>
+
+            <div class="flex flex-col bg-[#26225F] w-[380px]">
+                <div>
+                    <img class="w-[380px]" src="/images/teacher-giving-high-five.png" alt="" />
+                </div>
+                <div class="mx-12 pt-8 pb-5 text-white">
+                    <h1 class="pb-4 leading-[20px] poppins-semibold">
+                        Children have a right to an education, a quality
+                        <br />
+                        education.
+                    </h1>
+                    <p class="poppins-light text-xs pb-4">
+                        November 26, 2021
+                    </p>
+                    <a class="text-[#25A8D6] text-[8px]" href="#">READ MORE</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</x-guest-layout>
